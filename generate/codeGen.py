47954cd5cfa78c9b8631d0018b43f730c93f1787
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2009-09-28 15:17:53 -0600

    Baseline rapi gccxml

---
+++ generate/codeGen.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2006  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the BSD style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import os
+from TG.gccxml.codeAnalyzer import CodeAnalyzer
+from TG.gccxml.xforms.ctypes import AtomFilterVisitor, CCodeGenContext
+from TG.gccxml.xforms.ctypes import utils
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+analyzer = CodeAnalyzer(
+        inc=['inc', 'deps'],
+        src=['src/gen.c'], 
+        baseline=['src/baseline.c'])
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class FilterVisitor(AtomFilterVisitor):
+    def onFunction(self, item):
+        if not item.extern: return
+        if item.hasEllipsis():
+            return
+        self.select(item)
+
+    def onPPInclude(self, item):
+        print '"%s" includes "%s"' % (item.file.name, item.filename)
+
+    def onPPDefine(self, item):
+        if item.ident in self.filterConditionals:
+            return
+
+        if item.ident.startswith('Ce'):
+            # Grab all defines
+            #self.select(item)
+            print item.ident
+        else:
+            print item.ident
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    filterConditionals = set([])
+
+    def onPPConditional(self, item):
+        if not item.isOpening():
+            return 
+        if item.body in self.filterConditionals:
+            return
+
+        #self.select(item)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Main 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def main():
+    root = analyzer.loadModel()
+    context = CCodeGenContext(root)
+    context.atomFilter = FilterVisitor()
+
+    ciFilesByName = dict((os.path.basename(f.name), f) for f in context if f)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # setup imports
+
+    for ciFile in ciFilesByName.itervalues():
+        ciFile.importAll('_ctypes_rapi')
+
+    print ciFilesByName.keys()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # write output files
+
+    context.outputPath = '../raw/'
+    print
+    print "Writing out ctypes code:"
+    print "========================"
+    for ciFile in ciFilesByName.values():
+        print 'Writing:', ciFile.filename
+        ciFile.writeToFile()
+        print 'Done Writing:', ciFile.filename
+        print
+    print
+
+    utils.includeSupportIn(context.getOutputFilename('_ctypes_support.py'), copySource=True)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__=='__main__':
+    main()
+
